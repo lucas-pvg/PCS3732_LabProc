4b986fb65198792632cc3943b0bad2bd111d4ac8
asakuraLie <lie.asakura000@gmail.com>
2023-07-10 09:43:39 -0300

    🚨 Arrumando alguns warnings

--- emulador-didatico/src/App.js
+++ emulador-didatico/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a>Nosso Emulador Didatico de Instruções ARM</a>
+        <h1>Emulador yay</h1>
       </header>
     </div>
   );
