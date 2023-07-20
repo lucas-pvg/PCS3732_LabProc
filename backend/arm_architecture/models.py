db215a2241b709f009ec988e0bca035121d42bd7
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2023-07-20 01:05:59 -0300

    Cria registrador

--- backend/arm_architecture/models.py
+++ backend/arm_architecture/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Memory(models.Model):
+    pass
+
+
+class Register(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, unique=True)
+    label = models.CharField()
+    data_binary = models.CharField(
+        max_length=34, default="0b00000000000000000000000000000000"
+    )
+
+    @property
+    def data_as_integer(self):
+        return int(self.data_binary, 0)
