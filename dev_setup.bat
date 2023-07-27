a608326f3733258d10a381ace99be76e113565f0
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2023-07-27 19:41:45 -0300

    Altera script para incluir instalação do front

--- dev_setup.bat
+++ dev_setup.bat
@@ -1,7 +1,7 @@
 echo off
 cls
 echo.
-echo CERTIFIQUE-SE QUE TENS O PYTHON 3.10 INSTALADO
+echo CERTIFIQUE-SE QUE TENS O PYTHON 3.10 E O NVM INSTALADO
 echo.
 echo Se nao tiver, aperte Ctrl+C para sair e instale-o
 pause
@@ -14,3 +14,13 @@ echo.
 echo Crie seu superuser:
 echo.
 pipenv run python manage.py createsuperuser
+echo.
+cd ..
+nvm install 19.5.0
+nvm use 19.5.0
+cd frontend
+npm install
+echo.
+echo Feche esse terminal
+echo.
+echo Execute o comando `npm install` dentro da pasta frontend
