21ec83568e40743e8b387bb4483ff0e988d285f0
asakuraLie <lie.asakura000@gmail.com>
2023-08-13 20:46:47 -0300

    🐛 a

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -2,22 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
-function Navbar()
-    return (
-       <nav className="navbar">
-          <Link to="/" className="logo route">
-             <img src={require("../../images/cpu.png")} />
-             <span>PCS3732</span>
-          </Link>
- 
-          <ul>
-             <li><Link to="arithmetics" className="route">Aritméticas</Link></li>
-             <li><Link to="branch" className="route">Desvio</Link></li>
-             <li><Link to="quiz" className="route">Quiz</Link></li>
-             <li><Link to="moreInfo" className="route">Saiba mais</Link></li>
-             <li><Link to="about" className="route">Quem somos nós?</Link></li>
-          </ul>
-       </nav>
-    );
+function Navbar() {
+   return (
+      <nav className="navbar">
+         <Link to="/" className="logo route">
+            <img src={require("../../images/cpu.png")} />
+            <span>PCS3732</span>
+         </Link>
+
+         <ul>
+            <li><Link to="arithmetics" className="route">Aritméticas</Link></li>
+            <li><Link to="branch" className="route">Desvio</Link></li>
+            <li><Link to="quiz" className="route">Quiz</Link></li>
+            <li><Link to="otherInstructions" className="route">Saiba mais</Link></li>
+            <li><Link to="about" className="route">Quem somos nós?</Link></li>
+         </ul>
+      </nav>
+   );
+}
 
 export default Navbar;
