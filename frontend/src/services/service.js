7949126a4d925a166f0529e78cdfd7e16cdc1e31
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2023-08-15 03:29:36 -0300

    Atualiza services com outros endpoints

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -3,6 +3,12 @@ import { get, post } from "./axios";
 const Service = {
     getRegisterList() {
         return get("register/");
+    },
+    updateAllRegisters(data) {
+        return post("register/", data)
+    },
+    postInstruction(data) {
+        return post("operation/", data)
     }
 };
 
